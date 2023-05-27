3cbe44696c46db4e28028a3c236cccad3984e8ca
Shi Tong Kim <shitongkim28@gmail.com>
2023-05-27 19:38:07 +0800

    Set-Up

---
+++ src/main/java/com/BackItUp/orbital/model/wallet.java
@@ -0,0 +1,49 @@
+package com.BackItUp.orbital.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class wallet {
+
+    @Id
+    @GeneratedValue
+    private Integer WALLET_ID;
+    private Integer ACTIVE_BALANCE;
+    private Integer FROZEN_BALANCE;
+
+    public WALLET() {
+    }
+
+    public WALLET(Integer activeBalance, Integer frozenBalance) {
+        this.ACTIVE_BALANCE = activeBalance;
+        this.FROZEN_BALANCE = frozenBalance;
+    }
+
+    public Integer getWallet_ID() {
+        return WALLET_ID;
+    }
+
+    public void setWallet_ID(Integer wallet_ID) {
+        this.WALLET_ID = wallet_ID;
+    }
+
+    public Integer getActiveBalance() {
+        return ACTIVE_BALANCE;
+    }
+
+    public void setActiveBalance(Integer activeBalance) {
+        this.ACTIVE_BALANCE = activeBalance;
+    }
+
+    public Integer getFrozenBalance() {
+        return FROZEN_BALANCE;
+    }
+
+    public void setFrozenBalance(Integer frozenBalance) {
+        this.FROZEN_BALANCE = frozenBalance;
+    }
+
+
+}
