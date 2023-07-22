6c4c1854efdc09042d0e7656e8f7e06723d0a10b
cuison <e0969947@u.nus.edu>
2023-07-23 01:16:55 +0800

    update resource page

--- backitup-front/src/pages/get-started/Resources.js
+++ backitup-front/src/pages/get-started/Resources.js
@@ -9,7 +9,7 @@ export default function Resources({setPageTitle}) {
 
   return (
     <div>
-        <div class="px-4 pt-5 my-5 text-center border-bottom">
+        <div class="px-4 my-5 text-center border-bottom">
             <h1 class="hero-title display-4 fw-bold">We're here to help.</h1>
             <div class="col-lg-6 mx-auto">
                 <p class="lead mb-4">A collection of our favorite entrepreneurship resources, curated for you.</p>
@@ -38,6 +38,15 @@ export default function Resources({setPageTitle}) {
             </div>
             <div className='col-md-6 px-5' style={{textAlign: "left"}}>
                 <h3 class="display-10 fw-bold">Kim's Picks</h3>
+                <h3 class="display-10 fw-bold">Jacob's Picks</h3>
+                <p>Man like Geoff.</p>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/gcevHkNGrWQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                <hr />
+                <p>One of the most important skills you must have.</p>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/17XZGUX_9iM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                <hr />
+                <p>A classic.</p>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/yQLhWtgAT0A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
             </div>
             
         </div>
